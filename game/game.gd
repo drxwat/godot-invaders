6d2ffc80c8ec174adb805f0efb574cad0ecdb62d
Art <14210921+tyomklz@users.noreply.github.com>
2024-02-11 08:22:23 +0300

    further improvements
    - add enemy bounce on death
    
    - fix points never reset after game over
    
    - fix rockets or enemy bullets not being removed after scene reload on game over

--- game/game.gd
+++ game/game.gd
@@ -4,6 +4,7 @@ const GAME_OVER_SCENE = preload("res://ui/game_over/game_over.tscn")
 
 func _init():
 	Globals.lives = Globals.max_lives
+	Globals.points = 0
 
 func _ready():
 	Events.lives_changed.connect(func(lives): check_game_over())
@@ -14,9 +15,13 @@ func _ready():
 func check_game_over():
 	var enemies = get_tree().get_nodes_in_group("enemy")
 	var enemy_group = get_tree().get_first_node_in_group("enemy_group")
-	if Globals.lives <= 0 or enemies.size() <= 1 or enemy_group.global_position.y > 140:
+	if Globals.lives <= 0 or enemies.size() <= 0 or enemy_group.global_position.y > 140:
 		show_game_over()
 
 func show_game_over():
+	for rocket in get_tree().get_nodes_in_group("rocket"):
+		rocket.queue_free()
+	for enemy_bullet in get_tree().get_nodes_in_group("enemy_bullet"):
+		enemy_bullet.queue_free()
 	add_child(GAME_OVER_SCENE.instantiate())
 	get_tree().paused = true
