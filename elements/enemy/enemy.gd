a5646c95029517d38237fa0d7f30bad1be4a7de2
Art <14210921+tyomklz@users.noreply.github.com>
2024-02-11 05:31:40 +0300

    fix enemy dependency on enemy group

--- elements/enemy/enemy.gd
+++ elements/enemy/enemy.gd
@@ -10,7 +10,7 @@ var direction_changed := false
 func _physics_process(delta):
 	if not direction_changed:
 		if raycast_left.is_colliding() or raycast_right.is_colliding():
-			get_parent().change_direction()
+			get_tree().get_first_node_in_group("enemy_group").change_direction()
 			direction_changed = true
 	if not raycast_left.is_colliding() and not raycast_right.is_colliding():
 		direction_changed = false
